fd558f9063e6a154492ee3aebd442b111ceaae4e
Dino <dino.solano@riswit.it>
2019-04-13 17:37:30 +0200

    first commit with helloworldServerNode and Singleton pattern Design

---
+++ SingletonObject/app.js
@@ -0,0 +1,15 @@
+const fund = require('./bank');
+const atm1 = require('./bank');
+const atm2 = require('./bank');
+
+fund.deposite(10000);
+
+atm1.deposite(20);
+atm2.withdraw(120);
+
+console.log('total-atm1: ${atm1.total()}');
+console.log('total-atm2: ${atm2.total()}');
+
+fund.deposite(2000);
+
+console.log('total-fund: ${fund.total()}');
---
+++ SingletonObject/bank.js
@@ -0,0 +1,20 @@
+let cash = 0;
+
+const Bank = {
+    deposit(amount){
+        cash += amount;
+        return cash;
+    },
+    withdraw(amount){
+        if (amount <= cash){
+            cash -= amount;
+        } else {
+            return false;
+        }
+    },
+    total(){
+        return cash;
+    }
+}
+
+module.export = Bank;
