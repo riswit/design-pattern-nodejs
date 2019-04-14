67773e91f123e025da02535eddba9023f7d63014
Dino <dino.solano@riswit.it>
2019-04-14 13:24:18 +0200

    Simple Factory Pattern Design

---
+++ SimpleFactory/BMW.js
@@ -0,0 +1,8 @@
+const Car = require('./default_car');
+ 
+class BMW extends Car {
+    constructor() {
+        super('BMW')
+    }
+}
+module.exports = BMW;
---
+++ SimpleFactory/Mercedes.js
@@ -0,0 +1,8 @@
+const Car = require('./default_car');
+ 
+class Mercedes extends Car {
+    constructor() {
+        super('Mercedes')
+    }
+}
+module.exports = Mercedes;
